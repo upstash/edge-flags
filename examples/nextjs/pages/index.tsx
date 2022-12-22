e2749b4e83302dd84f17043e83a81e1b5c06e3dd
Andreas Thomas <dev@chronark.com>
2022-12-22 09:19:51 +0100

    feat: example

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,85 +1,91 @@
 import {
-	Card,
-	Title,
-	Text,
-	ColGrid,
-	Col,
-	Block,
-	Metric,
-	Badge,
-	Toggle,
-	ToggleItem,
-	Flex,
+  Card,
+  Title,
+  Text,
+  ColGrid,
+  Col,
+  Block,
+  Metric,
+  Badge,
+  Toggle,
+  ToggleItem,
+  Flex,
 } from "@tremor/react";
 import { useFlag } from "@upstash/edge-flags";
 
 export default function Example() {
-	const { isEnabled, isLoading, error, latency } = useFlag("my-flag");
-	return (
-		<main
-			style={{
-				margin: "8rem",
-			}}
-		>
-			<Title>@upstash/edge-flags</Title>
-			<Text color="blue">
-				<a
-					href="https://console-git-feature-flag-upstash.vercel.app/edge-flags"
-					target="_blank"
-					rel="noreferrer"
-				>
-					console.upstash.com/edge-flags
-				</a>
-			</Text>
+  const { isEnabled, isLoading, error, latency } = useFlag("my-flag");
+  return (
+    <main
+      style={{
+        margin: "8rem",
+      }}
+    >
+      <Title>@upstash/edge-flags</Title>
+      <Text color="blue">
+        <a
+          href="https://console-git-feature-flag-upstash.vercel.app/edge-flags"
+          target="_blank"
+          rel="noreferrer"
+        >
+          console.upstash.com/edge-flags
+        </a>
+      </Text>
 
-			<ColGrid
-				numColsMd={2}
-				numColsLg={4}
-				gapX="gap-x-6"
-				gapY="gap-y-6"
-				marginTop="mt-6"
-			>
-				<Card>
-					<Block truncate={true}>
-						<Text>Loading</Text>
-						<Metric truncate={true}>
-							{isLoading.toString().toUpperCase()}
-						</Metric>
-					</Block>
-				</Card>
-				<Card>
-					<Block truncate={true}>
-						<Text>Error</Text>
-						<Metric truncate={true}>{error ?? "-"}</Metric>
-					</Block>
-				</Card>
-				<Card>
-					<Block truncate={true}>
-						<Text>Enabled</Text>
-						<Metric truncate={true}>
-							{isEnabled?.toString().toUpperCase()}
-						</Metric>
-					</Block>
-				</Card>
-				<Card>
-					<Block truncate={true}>
-						<Text>Latency [ms]</Text>
-						<Metric truncate={true}>
-							{latency
-								? Intl.NumberFormat(undefined, {
-										compactDisplay: "short",
-								  }).format(latency)
-								: undefined}
-						</Metric>
-					</Block>
-				</Card>
-			</ColGrid>
+      <ColGrid
+        numColsMd={2}
+        numColsLg={4}
+        gapX="gap-x-6"
+        gapY="gap-y-6"
+        marginTop="mt-6"
+      >
+        <Card>
+          <Block truncate={true}>
+            <Text>Loading</Text>
+            <Metric truncate={true}>
+              {isLoading.toString().toUpperCase()}
+            </Metric>
+          </Block>
+        </Card>
+        <Card>
+          <Block truncate={true}>
+            <Text>Error</Text>
+            <Metric truncate={true}>{error ?? "-"}</Metric>
+          </Block>
+        </Card>
+        <Card>
+          <Block truncate={true}>
+            <Text>Enabled</Text>
+            <Metric truncate={true}>
+              {isEnabled?.toString().toUpperCase()}
+            </Metric>
+          </Block>
+        </Card>
+        <Card>
+          <Block truncate={true}>
+            <Text>Latency</Text>
+            <Flex>
 
-			<Block marginTop="mt-6">
-				<Card>
-					<div className="h-80" />
-				</Card>
-			</Block>
-		</main>
-	);
+              <Metric truncate={true}>
+                {latency
+                  ? Intl.NumberFormat(undefined, {
+                    compactDisplay: "short",
+                  }).format(latency)
+                  : undefined}
+              </Metric>
+              <Text>
+                ms
+              </Text>
+            </Flex>
+          </Block>
+        </Card>
+      </ColGrid>
+
+      <Block marginTop="mt-6">
+        <Card>
+          <div className="h-80" />
+        </Card>
+      </Block>
+    </main>
+  );
 }
