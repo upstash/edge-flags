7678bbe1146a9079293777dfacae169603b6a6e5
Andreas Thomas <dev@chronark.com>
2023-01-19 12:24:43 +0100

    feat: add telemetry and clean up (#9)

--- cmd/set-version.js
+++ cmd/set-version.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
+const path = require("path");
 
-const file = process.argv[2]; // path to package.json
+const root = process.argv[2]; // path to project root
 const version = process.argv[3].replace(/^v/, ""); // new version
 
-console.log(`Updating version=${version} in ${file}`);
+console.log(`Updating version=${version} in ${root}`);
 
-const content = JSON.parse(fs.readFileSync(file, "utf-8"));
+const content = JSON.parse(fs.readFileSync(path.join(root, "package.json"), "utf-8"));
 
 content.version = version;
 
-fs.writeFileSync(file, JSON.stringify(content, null, 2));
+fs.writeFileSync(path.join(root, "package.json"), JSON.stringify(content, null, 2));
+fs.writeFileSync(path.join(root, "src", "version.ts"), `export const VERSION = "${version}";`);
