d9187d8285788dee0805dc7c5f400bc02bcf93e9
Andreas Thomas <dev@chronark.com>
2023-01-23 11:03:48 +0100

    docs: middleware example

---
+++ examples/middleware-rewrites/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Client as EdgeFlags } from "@upstash/edge-flags";
+
+const edgeFlags = new EdgeFlags({ redis: Redis.fromEnv() });
+export default async function middleware(req: NextRequest): Promise<NextResponse> {
+  const enabled = await edgeFlags.getFlag("eu-countries", req.geo ?? {});
+
+  if (!enabled) {
+    const url = new URL(req.url);
+    url.pathname = "/blocked";
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/",
+};
