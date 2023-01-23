19a275ff84681ed317279d90a13fe1b2e5698251
Andreas Thomas <dev@chronark.com>
2023-01-23 10:30:31 +0100

    feat: add client

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -1,98 +1,38 @@
-import { useState } from "react";
+import { Redis } from "@upstash/redis";
+import { Admin } from "./admin";
+import { evaluate } from "./evaluation";
+import { EvalRequest } from "./rules";
+import { Environment } from "./types";
 
-export type UseFlag = {
-  isLoading: boolean;
-  error: string | null;
-  isEnabled: boolean | null;
-  refresh: () => Promise<void>;
-  /**
-   * For development purposes only
-   *
-   * This can change at any time
-   */
-  debug: {
-    latency: {
-      total: number | null;
-      edge: number | null;
-      redis: number | null;
-    };
-
-    cache: {
-      memory: string | null;
-      vercel: string | null;
-    };
-  };
+export type ClientConfig = {
+  redis: Redis;
+  environment?: Environment;
 };
 
-export function useFlag(flag: string, attributes?: Record<string, string | number | boolean>): UseFlag {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [isEnabled, setIsEnabled] = useState<boolean | null>(null);
-  const [latency, setLatency] = useState<number | null>(null);
-  const [redisLatency, setRedisLatency] = useState<number | null>(null);
-  const [edgeLatency, setEdgeLatency] = useState<number | null>(null);
-  const [memoryCacheHit, setMemoryCacheHit] = useState<string | null>(null);
-  const [vercelCacheHit, setVercelCacheHit] = useState<string | null>(null);
+export class Client {
+  private admin: Admin;
+  private readonly environment: Environment;
 
-  const getFlag = async () => {
-    setError(null);
-    setMemoryCacheHit(null);
-    setVercelCacheHit(null);
+  constructor(config: ClientConfig) {
+    this.admin = new Admin({ redis: config.redis });
+    this.environment = config.environment ?? (process.env.VERCEL_ENV as Environment) ?? "development";
+  }
 
-    const now = Date.now();
-    try {
-      setIsLoading(true);
-      const params = new URLSearchParams();
+  async getFlag(flagName: string, req: EvalRequest): Promise<boolean> {
+    const flag = await this.admin.getFlag(flagName, this.environment);
 
-      attributes ??= {};
-      attributes["_flag"] = flag;
-      if (attributes) {
-        for (const [k, v] of Object.entries(attributes)) {
-          params.set(k, v.toString());
-        }
-      }
-      const res = await fetch(`/api/edge-flags?${params.toString()}`);
-      if (!res.ok) {
-        setError(await res.text());
-        return;
-      }
-      const json = (await res.json()) as { value: boolean };
-      setVercelCacheHit(res.headers.get("X-Vercel-Cache"));
-      setMemoryCacheHit(res.headers.get("X-Edge-Flags-Cache"));
-      setRedisLatency(parseInt(res.headers.get("X-Redis-Latency") ?? "-1"));
-      setEdgeLatency(parseInt(res.headers.get("X-Edge-Latency") ?? "-1"));
-      setIsEnabled(json.value);
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        throw err;
-      }
-    } finally {
-      setLatency(Date.now() - now);
-      setIsLoading(false);
+    if (!flag) {
+      throw new Error(`Flag ${flagName} not found`);
     }
-  };
 
-  // useEffect(() => {
-  //   getFlag();
-  // }, [flag, attributes]);
+    const hash = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(JSON.stringify(req)));
+    const hashSum = Array.from(new Uint8Array(hash)).reduce((sum, x) => {
+      sum += x;
+      return sum;
+    }, 0);
 
-  return {
-    isLoading,
-    error,
-    isEnabled,
-    refresh: getFlag,
-    debug: {
-      latency: {
-        total: latency,
-        edge: edgeLatency,
-        redis: redisLatency,
-      },
-      cache: {
-        vercel: vercelCacheHit,
-        memory: memoryCacheHit,
-      },
-    },
-  };
+    const percentage = hashSum % 100;
+
+    return evaluate(flag, percentage, req);
+  }
 }
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,5 +2,6 @@ export * from "./types";
 export * from "./handler";
 export * from "./admin";
 export * from "./environment";
+export * from "./hook";
 export * from "./client";
 export * from "./middleware";
