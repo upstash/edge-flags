08ccb57a8c75d364703d0eb4c1d2fd3ecf93eab4
ytkimirti <yusuftaha9@gmail.com>
2024-06-26 19:49:43 +0300

    update example

--- examples/middleware-rewrites/middleware.ts
+++ examples/middleware-rewrites/middleware.ts
@@ -2,11 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
 import { Client as EdgeFlags } from "@upstash/edge-flags";
 
-const edgeFlags = new EdgeFlags({ redis: Redis.fromEnv() });
+const edgeFlags = new EdgeFlags({
+  redis: Redis.fromEnv(),
+  // You can enable debug mode to see the logs
+  debug: true,
+});
 
 export default async function middleware(
   req: NextRequest
 ): Promise<NextResponse> {
+  // req.geo object is provided by nextjs
+  // it only works when deployed to Vercel
   const enabled = await edgeFlags
     .getFlag("eu-countries", req.geo ?? {})
     .catch((err) => {
